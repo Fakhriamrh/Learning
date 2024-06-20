591743355712474f55315f3fb5e7c492764e5c86
Fakhriamrh <16fakhriamrullah@gmail.com>
2024-06-20 10:44:41 +0700

    cek3

--- Test/test_Sendbird.py
+++ Test/test_Sendbird.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 
