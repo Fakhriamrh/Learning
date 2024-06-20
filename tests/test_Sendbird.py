ebfae0f8a7d2b7379ff4746fb237397cc4ba215b
Fakhriamrh <16fakhriamrullah@gmail.com>
2024-06-20 16:22:57 +0700

    ceklg

--- tests/test_Sendbird.py
+++ tests/test_Sendbird.py
@@ -103,11 +103,11 @@ def test_sending_message_and_attachments_by_user2(setup_driver):
     # Test sending messages and attachments by User 2
     send_plain_text(setup_driver, "Send Plain Text")
 
-    file_path_picture = '/Users/Fvkrey/Desktop/SendbirdTestPict.png'
+    file_path_picture = '/Learning/tests/images/SendbirdTestPict.png'
     send_picture(setup_driver, file_path_picture)
     time.sleep(3)
 
-    file_path_text = '/Users/Fvkrey/Desktop/SendbirdTest.txt'
+    file_path_text = '/Learning/tests/text_files/SendbirdTest.txt'
     send_text_file(setup_driver, file_path_text)
     time.sleep(3)
     
