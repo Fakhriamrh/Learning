f270dadd77ffeb94a0aaf0cc92bcb1ae34f101ba
Fakhriamrh <16fakhriamrullah@gmail.com>
2024-06-23 14:05:12 +0700

    modified:   tests/test_Sendbird.py

--- tests/test_Sendbird.py
+++ tests/test_Sendbird.py
@@ -1,5 +1,5 @@
-import os
 import pytest
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,7 +8,6 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
 
 # Fixture for WebDriver initialization
 @pytest.fixture(scope="module")
@@ -21,8 +20,8 @@ def setup_driver():
     yield driver
     driver.quit()
 
-# Function for logging in and initial user settings 1
-def login_and_setup1(driver, app_id, user_id, nickname):
+# Function for logging in and initial user settings
+def login_and_setup(driver, app_id, user_id, nickname):
     driver.get("https://sendbird-uikit-react.netlify.app/url-builder")
     time.sleep(3)  # Wait for the page to load
 
@@ -32,26 +31,18 @@ def login_and_setup1(driver, app_id, user_id, nickname):
     driver.find_element(By.CSS_SELECTOR, "button.sticky-bottom-button[form='builder']").click()
     time.sleep(3)  # Wait for the page to load
 
-
 # Function to send text messages
 def send_plain_text(driver, text):
-    Send_text = driver.find_element(By.ID, "sendbird-message-input-text-field")
-    Send_text.send_keys(text)
-    Send_text.send_keys(Keys.ENTER)
+    send_text = driver.find_element(By.ID, "sendbird-message-input-text-field")
+    send_text.send_keys(text)
+    send_text.send_keys(Keys.ENTER)
     time.sleep(3)  # Wait for the page to load
 
-# Function to send images
-def send_picture(driver, file_path_picture):
-    file_path_picture = os.path.abspath(file_path_picture)
-    Send_Picture = driver.find_element(By.CLASS_NAME, "sendbird-message-input--attach-input")
-    Send_Picture.send_keys(file_path_picture)
-    time.sleep(3)  # Wait for the page to load
-
-# Function to send text files
-def send_text_file(driver, file_path_text):
-    file_path_text = os.path.abspath(file_path_text)
-    Send_File = driver.find_element(By.CLASS_NAME, "sendbird-message-input--attach-input")
-    Send_File.send_keys(file_path_text)
+# Function to send images or files
+def send_file(driver, file_path):
+    file_path = os.path.abspath(file_path)
+    send_file = driver.find_element(By.CLASS_NAME, "sendbird-message-input--attach-input")
+    send_file.send_keys(file_path)
     time.sleep(3)  # Wait for the page to load
 
 # Test cases using Pytest
@@ -60,14 +51,13 @@ def test_create_user1(setup_driver):
     app_id = "37C8DB25-8B44-435F-A528-5BA9B9965FD0"
     user_id = "1Testing"
     nickname = "User1"
-    login_and_setup1(setup_driver, app_id, user_id, nickname)
+    login_and_setup(setup_driver, app_id, user_id, nickname)
     # Change tab for User 1
     setup_driver.execute_script("window.open('');")
-    Tab_For_User1 = setup_driver.window_handles[1]
-    setup_driver.switch_to.window(Tab_For_User1)
+    tab_for_user1 = setup_driver.window_handles[1]
+    setup_driver.switch_to.window(tab_for_user1)
     setup_driver.get("https://sendbird-uikit-react.netlify.app/group_channel?appId=37C8DB25-8B44-435F-A528-5BA9B9965FD0&userId=1Testing&nickname=User1")
 
-    
 def test_create_user2(setup_driver):
     # Setting User 2
     setup_driver.switch_to.window(setup_driver.window_handles[0])
@@ -75,20 +65,11 @@ def test_create_user2(setup_driver):
     app_id2 = "37C8DB25-8B44-435F-A528-5BA9B9965FD0"
     user_id2 = "2Testing"
     nickname2 = "User2"
-    Textbox_name = setup_driver.find_element(By.NAME, "appId")
-    Textbox_name.clear()
-    Textbox_name.send_keys(app_id2)
-    Textbox_User = setup_driver.find_element(By.NAME, "userId")
-    Textbox_User.clear()
-    Textbox_User.send_keys(user_id2)
-    Textbox_nickname = setup_driver.find_element(By.NAME, "nickname")
-    Textbox_nickname.clear()
-    Textbox_nickname.send_keys(nickname2)
-    setup_driver.find_element(By.CSS_SELECTOR, "button.sticky-bottom-button[form='builder']").click()
+    login_and_setup(setup_driver, app_id2, user_id2, nickname2)
     # Change tab for User 2
     setup_driver.execute_script("window.open('');")
-    Tab_For_User2 = setup_driver.window_handles[2]
-    setup_driver.switch_to.window(Tab_For_User2)
+    tab_for_user2 = setup_driver.window_handles[2]
+    setup_driver.switch_to.window(tab_for_user2)
     setup_driver.get("https://sendbird-uikit-react.netlify.app/group_channel?appId=37C8DB25-8B44-435F-A528-5BA9B9965FD0&userId=2Testing&nickname=User2")
     time.sleep(3)
     setup_driver.find_element(By.XPATH, "//div[contains(@class,'sendbird-icon sendbird-icon-create sendbird-icon-color--primary')]//*[name()='svg']").click()
@@ -98,7 +79,7 @@ def test_create_user2(setup_driver):
     
     # Find the scrollable element
     scrollable_element = WebDriverWait(setup_driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//div[@class='sendbird-create-channel--scroll']"))
+        EC.presence_of_element_located((By.XPATH, "//div[@class='sendbird-create-channel--scroll']"))
     )   
 
     # Define the target checkbox element
@@ -112,29 +93,28 @@ def test_create_user2(setup_driver):
                 checkbox_element.click()
                 break
         except:
-        # Scroll down the scrollable element
-            setup_driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;", scrollable_element)
+            setup_driver.execute_script("arguments[0].scrollTop += arguments[0].offsetHeight;", scrollable_element)
             time.sleep(5)  # Adjust sleep time if necessary
         
     setup_driver.find_element(By.XPATH, "//button[contains(@class,'sendbird-button--primary sendbird-button--big')]").click()
-    time.sleep(10)  
+    time.sleep(3)  
 
 def test_sending_message_and_attachments_by_user2(setup_driver): 
     # Test sending messages and attachments by User 2
     send_plain_text(setup_driver, "Send Plain Text")
 
-    file_path_picture = 'tests/images/SendbirdTestPict.png'
-    send_picture(setup_driver, file_path_picture)
+    file_path = 'tests/images/SendbirdTestPict.png'
+    send_file(setup_driver, file_path)
     time.sleep(3)
 
-    file_path_text = 'tests/text_files/SendbirdTest.txt'
-    send_text_file(setup_driver, file_path_text)
+    file_path = 'tests/text_files/SendbirdTest.txt'
+    send_file(setup_driver, file_path)
     time.sleep(3)
     
 def test_switch_tab_to_user1(setup_driver): 
     # Switch tab to user 1 for validation
-    Tab_For_User1 = setup_driver.window_handles[1]
-    setup_driver.switch_to.window(Tab_For_User1)
+    tab_for_user1 = setup_driver.window_handles[1]
+    setup_driver.switch_to.window(tab_for_user1)
     setup_driver.find_element(By.CLASS_NAME, 'sendbird-channel-preview[tabindex="0"]').click()
     
 def test_validate_message_and_attachments_by_user1(setup_driver):
@@ -145,7 +125,7 @@ def test_validate_message_and_attachments_by_user1(setup_driver):
     assert message_text == expected_message, f"Expected message '{expected_message}' but found '{message_text}'"
 
     # Image validation
-    image_element = setup_driver.find_element(By.XPATH, "//div[@class='sendbird-image-renderer__image']").click()
+    setup_driver.find_element(By.XPATH, "//div[@class='sendbird-image-renderer__image']").click()
     img_element = setup_driver.find_element(By.XPATH, "//img[@alt='SendbirdTestPict.png']")
     alt_attribute = img_element.get_attribute('alt')
     expected_alt_text = 'SendbirdTestPict.png'
@@ -159,4 +139,3 @@ def test_validate_message_and_attachments_by_user1(setup_driver):
         assert span_text == expected_text, f"Expected text '{expected_text}' but found '{span_text}'"
     except Exception as e:
         pytest.fail(f"Error occurred: {e}")
-
